59f7578320aff394738aac7048667113dfb2eeb6
impostor-dot-com <nohurrying@gmail.com>
2026-02-05 14:50:36 +0700

    Blablacar - Start code

---
+++ services/location_service.dart
@@ -0,0 +1,12 @@
+import 'package:blabla/data/dummy_data.dart';
+
+import '../models/ride/locations.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class LocationsService {
+  static const List<Location> availableLocations =
+      fakeLocations; // TODO for now fake data
+}
---
+++ services/rides_service.dart
@@ -0,0 +1,10 @@
+import '../data/dummy_data.dart';
+import '../models/ride/ride.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class RidesService {
+  static List<Ride> allRides = fakeRides;
+}
