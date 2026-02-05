1a05a2e25879531d6f6abf65199db07d7d0808f2
impostor-dot-com <nohurrying@gmail.com>
2026-02-05 15:44:56 +0700

    BLA 001 - Rides Service - Filter rides methods

--- services/rides_service.dart
+++ services/rides_service.dart
@@ -1,10 +1,38 @@
 import '../data/dummy_data.dart';
+import '../models/ride/locations.dart';
 import '../models/ride/ride.dart';
 
-////
-///   This service handles:
-///   - The list of available rides
-///
 class RidesService {
-  static List<Ride> allRides = fakeRides;
+  static List<Ride> availableRides = fakeRides; // TODO for now fake data
+
+  //
+  //  filter the rides starting from given departure location
+  //
+  static List<Ride> filterByDeparture(Location departure) {
+    return availableRides.where((ride) => ride.departureLocation == departure).toList(); //filter the rides that have the departure location
+  }
+
+  //
+  //  filter the rides starting for the given requested seat number
+  //
+  static List<Ride> filterBySeatRequested(int requestedSeat) {
+    return availableRides.where((ride) => ride.availableSeats >= requestedSeat).toList(); //filter the rides that have the same number of available seats of requested seats or more
+  }
+
+  //
+  //  filter the rides   with several optional criteria (flexible filter options)
+  //
+  static List<Ride> filterBy({Location? departure, int? seatRequested}) {
+    return availableRides.where((ride) {
+      if(departure != null && ride.departureLocation != departure){
+        return false;
+      } 
+      
+      if(seatRequested != null && ride.availableSeats < seatRequested){
+        return false;
+      }
+
+      return true;
+    }).toList();
+  }
 }
